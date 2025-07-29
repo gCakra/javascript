451b6986085446e9da0378e7530602c4d325b880
gCakra <gardagcwa5400@gmail.com>
2025-07-29 22:34:33 +0700

    menyelesaikan tutorial if else

---
+++ app.js
@@ -0,0 +1,22 @@
+// alert(1 + 1);
+// console.log("hello world");
+
+// console.log("sebelum pembanding");
+
+// if (1 + 1 === 3) {
+//     console.log("benar");
+// }
+
+// console.log("sesudah pembanding");
+
+let angka = Math.random();
+console.log(angka);
+
+if (angka >= 0.5) {
+  console.log("angka lebih besar dari lima");
+}
+
+if (angka <= 0.5) {
+  console.log("angka lebih kecil dari lima");
+}
+
